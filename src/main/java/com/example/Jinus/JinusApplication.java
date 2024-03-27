c127c12a510c78002def37d64887e050d7c1741d
hykim02 <114382247+hykim02@users.noreply.github.com>
2024-03-27 21:35:45 +0900

    서버 연동 성공

---
+++ src/main/java/com/example/Jinus/JinusApplication.java
@@ -0,0 +1,13 @@
+package com.example.Jinus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JinusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JinusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Jinus/JinusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Jinus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JinusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
