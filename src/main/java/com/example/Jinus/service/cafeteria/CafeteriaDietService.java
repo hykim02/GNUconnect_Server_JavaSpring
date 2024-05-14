eb2d9814070de7e855b7c1f386212639c3d2e211
hykim02 <114382247+hykim02@users.noreply.github.com>
2024-05-14 18:39:37 +0900

    식단 데이터 가공 중

--- src/main/java/com/example/Jinus/service/cafeteria/CafeteriaDietService.java
+++ src/main/java/com/example/Jinus/service/cafeteria/CafeteriaDietService.java
@@ -29,14 +29,38 @@ public class CafeteriaDietService {
         logger.info("getCafeteriaDiet 실행");
 
         List<CafeteriaDietEntity> dishCategory = cafeteriaDietRepository.findCategoryOrType(date, time, cafeteriaId);
-        logger.info("dishCategory: {}", dishCategory);
+        HashMap<String, List<String>> CategoryMenuMap = new HashMap<>();
+        List<String> menuList = new ArrayList<>(); // 메뉴 담을 리스트 생성 및 초기화
+        List<String> categoryCheckList = new ArrayList<>(); // 카테고리 중복체크를 위한 리스트
 
         for (CafeteriaDietEntity dish : dishCategory) {
-            logger.info("dish: {}", dish);
+
             logger.info("dish_category: {}", dish.getDishCategory());
             logger.info("dish_type: {}", dish.getDishType());
             logger.info("dish_name: {}", dish.getDishName());
-        }
 
+            if (dish.getDishCategory() != null) {
+                if (!categoryCheckList.contains(dish.getDishCategory())) {
+                    categoryCheckList.add(dish.getDishCategory());
+                }
+                menuList.add(dish.getDishName());
+                break;
+            } else if (dish.getDishType() != null) {
+                if (!categoryCheckList.contains(dish.getDishType())) {
+                    categoryCheckList.add(dish.getDishType());
+                }
+                menuList.add(dish.getDishName());
+            } else {
+                categoryCheckList.add("menu");
+            }
+        }
+    }
+
+    public List<String> getDishName(List<CafeteriaDietEntity> dishCategory, List<String> menuList, String category) {
+        for ( CafeteriaDietEntity dish : dishCategory) {
+            if (dish.getDishCategory().equals(category)) {
+
+            }
+        }
     }
 }
