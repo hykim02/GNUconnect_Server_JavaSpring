62aa7c8acf17c75c0fe0e16373735a7acbed2039
hykim02 <hykim4435@naver.com>
2025-03-01 00:53:07 +0900

    [Test]NoticeRepositoryV2Test 성공

--- src/main/java/com/example/Jinus/repository/v2/notice/NoticeCategoryRepositoryV2.java
+++ src/main/java/com/example/Jinus/repository/v2/notice/NoticeCategoryRepositoryV2.java
@@ -3,10 +3,11 @@ package com.example.Jinus.repository.v2.notice;
 import com.example.Jinus.entity.notice.NoticeCategoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface NoticeCategoryRepositoryV2 extends JpaRepository<NoticeCategoryEntity, Integer> {
     @Query("SELECT n FROM NoticeCategoryEntity n WHERE n.departmentId = :departmentId")
-    List<NoticeCategoryEntity> findCategoryListByDepartmentId(int departmentId);
+    List<NoticeCategoryEntity> findCategoryListByDepartmentId(@Param("departmentId")int departmentId);
 }
