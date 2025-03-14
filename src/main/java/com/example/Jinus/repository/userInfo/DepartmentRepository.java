2d607bfdaf8c22cf2e2a5cf8d93ce6c6c9df218a
hykim02 <hykim4435@naver.com>
2025-03-14 18:03:09 +0900

    [Docs]v1 관련 파일들 삭제

--- src/main/java/com/example/Jinus/repository/userInfo/DepartmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Jinus.repository.userInfo;
-
-import com.example.Jinus.entity.userInfo.DepartmentEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DepartmentRepository extends JpaRepository<DepartmentEntity, Integer> {
-}
