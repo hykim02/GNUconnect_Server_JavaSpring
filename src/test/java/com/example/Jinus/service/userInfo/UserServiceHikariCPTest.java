cddfdcf46ecffa284723f7dabe02be2363a0f743
hykim02 <hykim4435@naver.com>
2025-04-22 01:30:11 +0900

    [Test]hikariCP 멀티스레드 환경 테스트 (#37)
    * [Fix]Test 코드 수정
    
    * [Refactor]CafeteriaServiceV2 단일 책임 원칙 적용하여 클래스 분리
    
    * Update Java CI-CD.yml
    
    dependency submission code removed
    
    * [Refactor]CampusServiceV2 클래스 분리
    
    * [Refactor]DietServiceV2 서비스 클래스 분리
    
    * [fix]Cafeteria Cacheable 주석처리
    
    * [Refactor]캐시 적용 및 중복로직 최적화
    
    * [fix]userService에서 발생한 hikariCP connection error 찾는 중
    
    * [Test]HikariCP 멀티스레드 환경 테슽

---
+++ src/test/java/com/example/Jinus/service/userInfo/UserServiceHikariCPTest.java
@@ -0,0 +1,75 @@
+package com.example.Jinus.service.userInfo;
+
+import com.example.Jinus.service.v2.userInfo.UserServiceV2;
+import com.zaxxer.hikari.HikariDataSource;
+import com.zaxxer.hikari.HikariPoolMXBean;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import javax.sql.DataSource;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Slf4j
+@SpringBootTest
+@ActiveProfiles("test")
+public class UserServiceHikariCPTest {
+
+    @Autowired
+    private UserServiceV2 userService;
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Test
+    @DisplayName("멀티스레드 10개 생성 후 id 조회")
+    void hikariConnectionTimeoutTest() throws InterruptedException {
+        int threadCount = 10;
+        ExecutorService executor = Executors.newFixedThreadPool(threadCount);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            final int userNo = i;
+            executor.submit(() -> {
+                try {
+                    log.info(">> [{}] 서비스 호출 시작", userNo);
+                    userService.selectCampusIdTest("user" + userNo);
+                    log.info(">> [{}] 서비스 호출 완료", userNo);
+                } catch (Exception e) {
+                    log.error(">> [{}] 예외 발생: {}", userNo, e.getMessage());
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        // 커넥션 풀 상태 모니터링 (2초마다 찍기)
+        for (int i = 0; i < 5; i++) {
+            printHikariStatus();
+            Thread.sleep(2000);
+        }
+
+        latch.await();
+        executor.shutdown();
+    }
+
+    private void printHikariStatus() {
+        if (!(dataSource instanceof HikariDataSource hikariDataSource)) {
+            log.warn("데이터소스는 Hikari가 아님: {}", dataSource.getClass());
+            return;
+        }
+
+        HikariPoolMXBean poolStats = hikariDataSource.getHikariPoolMXBean();
+        log.info("### HikariCP 상태 체크 -> 총: {}, 사용 중: {}, 유휴: {}, 대기중 쓰레드: {}",
+                poolStats.getTotalConnections(),
+                poolStats.getActiveConnections(),
+                poolStats.getIdleConnections(),
+                poolStats.getThreadsAwaitingConnection()
+        );
+    }
+}
