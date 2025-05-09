95b7613308195141c9ea4ec6af9c5250ccae1b52
hykim02 <hykim4435@naver.com>
2025-05-09 15:43:31 +0900

    [Fix]redis db 수정 위해 cacheable 주석처리

--- src/main/java/com/example/Jinus/service/userInfo/CollegeService.java
+++ src/main/java/com/example/Jinus/service/userInfo/CollegeService.java
@@ -1,6 +1,5 @@
 package com.example.Jinus.service.userInfo;
 
-import com.example.Jinus.entity.userInfo.CollegeEntity;
 import com.example.Jinus.repository.userInfo.CollegeRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,32 +7,10 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CollegeService {
-    private static final Logger logger = LoggerFactory.getLogger(CollegeService.class);
     private final CollegeRepository collegeRepository;
 
     public CollegeService(CollegeRepository collegeRepository) {
         this.collegeRepository = collegeRepository;
     }
 
-    public String getCollegeName(int collegeId) {
-        CollegeEntity collegeEntity = collegeRepository.findById(collegeId).orElse(null);
-
-        if (collegeEntity != null) {
-            return collegeEntity.getCollegeEng(); // 학과 영어 이름 리턴(테이블 찾기 위함)
-        } else {
-            logger.debug("CollegeService: collge를 찾을 수 없습니다.");
-            return null;
-        }
-    }
-
-    public boolean checkEtcValue(int collegeId) {
-        CollegeEntity collegeEntity = collegeRepository.findById(collegeId).orElse(null);
-        return collegeEntity.isEtcValue();
-    }
-
-    // collegeId에 해당하는 campusId 찾기
-    public int getCampusId(int collegeId) {
-        int campusId = collegeRepository.findCampusId(collegeId);
-        return campusId;
-    }
 }
