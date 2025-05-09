424f06fde6fda2f58b8c33b6252a4417a0e6a824
hykim02 <hykim4435@naver.com>
2025-05-10 01:46:47 +0900

    [Fix] Cacheable 활성화

--- src/main/java/com/example/Jinus/service/cafeteria/CafeteriaQueryService.java
+++ src/main/java/com/example/Jinus/service/cafeteria/CafeteriaQueryService.java
@@ -12,19 +12,19 @@ import org.springframework.stereotype.Service;
 public class CafeteriaQueryService {
     private final CafeteriaRepository cafeteriaRepositoryV2;
 
-//    @Cacheable(
-//            value = "cafeteriaId",
-//            key = "#p0 + '::' + #p1",
-//            unless = "#result == -1",
-//            cacheManager = "contentCacheManager")
+    @Cacheable(
+            value = "cafeteriaId",
+            key = "#p0 + '::' + #p1",
+            unless = "#result == -1",
+            cacheManager = "contentCacheManager")
     public int getCafeteriaId(String cafeteriaName, int campusId) {
         return cafeteriaRepositoryV2.findCafeteriaId(cafeteriaName, campusId).orElse(-1);
     }
 
-//    @Cacheable(
-//            value = "cafeteriaUrl",
-//            key = "#p0",
-//            cacheManager = "contentCacheManager")
+    @Cacheable(
+            value = "cafeteriaUrl",
+            key = "#p0",
+            cacheManager = "contentCacheManager")
     public String getImgUrl(int cafeteriaId) {
         return cafeteriaRepositoryV2.findImgUrlByCafeteriaId(cafeteriaId);
     }
